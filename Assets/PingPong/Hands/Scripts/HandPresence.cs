9c66cd1438441a94011059e7b84d697f9b751fd9
TusharVaid30 <tusharvaid30@gmail.com>
2021-10-11 17:53:53 +0530

    VR Hand Animation

---
+++ Assets/PingPong/Hands/Scripts/HandPresence.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR;
+using System.Collections.Generic;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacteristics;
+
+    private InputDevice targetDevice;
+
+    private Animator hand;
+    
+    private void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        if (devices.Count > 0)
+            targetDevice = devices[0];
+
+        hand = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float trigger);
+        if (trigger > .1f)
+            hand.SetFloat("Hold", trigger);
+        else
+            hand.SetFloat("Hold", 0f);
+    }
+}
