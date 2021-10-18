2a29b9b6a0ea2c02107019517ac4bdbcfc4731ab
TusharVaid30 <tusharvaid30@gmail.com>
2021-10-18 16:53:59 +0530

    Better AI

--- Assets/PingPong/Scripts/Opponent AI/OpponentAI.cs
+++ Assets/PingPong/Scripts/Opponent AI/OpponentAI.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 
 public class OpponentAI : MonoBehaviour
 {
+    public float speed;
+    public float distance;
     public Transform Ball;
     
     private void Update()
     {
-        Vector3 pos = transform.position;
-        pos.x = Ball.position.x;
-        pos.y = Ball.position.y;
-        transform.position = pos;
+        bool isCloseToBall = transform.position.z - Ball.position.z < 1f;
+
+        if (isCloseToBall)
+        {
+            Vector3 pos = transform.position;
+            pos.x = Ball.position.x;
+            pos.y = Ball.position.y;
+            transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+        }
     }
 }
