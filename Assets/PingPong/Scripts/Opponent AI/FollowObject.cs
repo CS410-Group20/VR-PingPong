562372ec89ea5723f9555d895af7de08a442e0b3
TusharVaid30 <tusharvaid30@gmail.com>
2021-10-18 18:46:35 +0530

    Minor AI Changes

---
+++ Assets/PingPong/Scripts/Opponent AI/FollowObject.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform followObject;
+
+    public float speed = 10f;
+    
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, followObject.position, speed * Time.deltaTime);
+    }
+}
