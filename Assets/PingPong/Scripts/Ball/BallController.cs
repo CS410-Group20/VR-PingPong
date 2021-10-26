78e2956a867ed1f1e7d835b8cde888239de140c2
TusharVaid30 <tusharvaid30@gmail.com>
2021-10-26 15:35:32 +0530

    Added Room

--- Assets/PingPong/Scripts/Ball/BallController.cs
+++ Assets/PingPong/Scripts/Ball/BallController.cs
@@ -1,15 +1,19 @@
+using System;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class BallController : MonoBehaviour
 {
     [SerializeField] private int difficulty = 1;
     [SerializeField] private float[] gravity;
     [SerializeField] private float[] speed;
+    [SerializeField] private float fakeBounce;
+    [SerializeField] Transform paddle;
     
     private Rigidbody rb;
     private Vector3 oldPosition;
     private float playerSpeed;
-    private float AiSpeed;
+    private float aiSpeed;
     
     private void Start()
     {
@@ -18,25 +22,36 @@ public class BallController : MonoBehaviour
         CalculateSpeed();
     }
 
+    private void Update()
+    {
+        print(paddle.transform.localRotation);
+    }
+
     private void CalculateSpeed()
     {
         Vector3 gravity = new Vector3(0f, this.gravity[difficulty], 0f);
         Physics.gravity = gravity;
-        playerSpeed = AiSpeed = speed[difficulty];
+        playerSpeed = aiSpeed = speed[difficulty];
     }
 
+    private void FakeBounce()
+    {
+        if (difficulty == 0)
+            rb.AddForce(0f, fakeBounce, 0f);
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Hit Area"))
         {
             rb.useGravity = true;
-            rb.velocity = -other.transform.up * playerSpeed * Time.fixedDeltaTime;
+            var hitArea = other.transform;
+            rb.velocity = -hitArea.up * playerSpeed * Time.fixedDeltaTime;
         }
-
-        if (other.transform.CompareTag("Wall"))
-        {
-            rb.velocity = other.transform.up * AiSpeed * Time.fixedDeltaTime;
-        }
+        else if (other.transform.CompareTag("Table"))
+            FakeBounce();
+        else if (other.transform.CompareTag("Wall"))
+            rb.velocity = other.transform.up * aiSpeed * Time.fixedDeltaTime;
     }
 
     private enum tags
